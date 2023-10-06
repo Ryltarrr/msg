e80b7b0c7c7b7c31b03fd6f3f4f60c8f6aafa837
Justin Guerré <j.guerre@inlead.fr>
2023-10-06 09:50:01 +0200

    listen on all addresses

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ async fn main() {
         .nest_service("/dist", ServeDir::new("dist"))
         .with_state(pool);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
